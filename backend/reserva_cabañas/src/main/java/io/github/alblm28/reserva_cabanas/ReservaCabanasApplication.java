9eca09b550c4c111c795c06f4d3d8aec84f7b912
alba <alballanomanrique28@gmail.com>
2026-04-02 19:14:24 +0200

    spring conectado a postgre

---
+++ backend/reserva_cabañas/src/main/java/io/github/alblm28/reserva_cabanas/ReservaCabanasApplication.java
@@ -0,0 +1,13 @@
+package io.github.alblm28.reserva_cabanas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaCabanasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaCabanasApplication.class, args);
+	}
+
+}
---
+++ backend/reserva_cabañas/src/test/java/io/github/alblm28/reserva_cabanas/ReservaCabanasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alblm28.reserva_cabanas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaCabanasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
